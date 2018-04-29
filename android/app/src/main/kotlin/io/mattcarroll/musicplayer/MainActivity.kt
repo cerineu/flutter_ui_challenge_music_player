c601b7735fad93be280577ac5d444dc2eeb210cf
Matt Carroll <me@mattcarroll.name>
2018-04-29 13:44:52 -0700

    Implemented visualizer.

--- android/app/src/main/kotlin/io/mattcarroll/musicplayer/MainActivity.kt
+++ android/app/src/main/kotlin/io/mattcarroll/musicplayer/MainActivity.kt
@@ -1,6 +1,10 @@
 package io.mattcarroll.musicplayer
 
 import android.os.Bundle
+import android.content.pm.PackageManager
+import android.Manifest
+import android.support.v4.content.ContextCompat
+import android.support.v4.app.ActivityCompat
 
 import io.flutter.app.FlutterActivity
 import io.flutter.plugins.GeneratedPluginRegistrant
@@ -9,5 +13,16 @@ class MainActivity(): FlutterActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     GeneratedPluginRegistrant.registerWith(this)
+  }
+
+  override fun onResume() {
+    super.onResume()
+
+    if (ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO)
+            != PackageManager.PERMISSION_GRANTED) {
+      ActivityCompat.requestPermissions(this,
+              arrayOf(Manifest.permission.RECORD_AUDIO),
+              1000);
+    }
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:fluttery_audio/fluttery_audio.dart';
+import 'package:meta/meta.dart';
 import 'package:music_player/bottom_controls.dart';
 import 'package:music_player/songs.dart';
+import 'package:music_player/theme.dart';
 import 'package:music_player/top_controls.dart';
 
 void main() => runApp(new MyApp());
@@ -90,6 +94,18 @@ class _MusicPlayerState extends State<MusicPlayer> {
             new Container(
               width: double.infinity,
               height: 125.0,
+              child: new Visualizer(
+                builder: (BuildContext context, List<int> fft) {
+                  return new CustomPaint(
+                    painter: new VisualizerPainter(
+                      fft: fft,
+                      height: 125.0,
+                      color: accentColor,
+                    ),
+                    child: new Container(),
+                  );
+                },
+              )
             ),
 
             // Song title, artist name, playback controls
@@ -99,4 +115,115 @@ class _MusicPlayerState extends State<MusicPlayer> {
       ),
     );
   }
+}
+
+class VisualizerPainter extends CustomPainter {
+
+  final List<int> fft;
+  final double height;
+  final Paint wavePaint;
+
+  VisualizerPainter({
+    this.fft = const [],
+    @required this.height,
+    color = Colors.black,
+  }) : wavePaint = new Paint()
+        ..color = color.withOpacity(0.75);
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    _renderWaves(canvas, size);
+  }
+
+  void _renderWaves(Canvas canvas, Size size) {
+    final histogramLow = _createHistogram(fft, 15, 0, ((fft.length) / 4).floor());
+    final histogramHigh = _createHistogram(fft, 15, (fft.length / 4).ceil(), (fft.length / 2).floor());
+
+    _renderHistogram(canvas, size, histogramLow);
+    _renderHistogram(canvas, size, histogramHigh);
+  }
+
+  void _renderHistogram(Canvas canvas, Size size, List<int> histogram) {
+    if (histogram.length == 0) {
+      return;
+    }
+
+    final pointsToGraph = histogram.length;
+    final widthPerSample = (size.width / (pointsToGraph - 2)).floor(); // -2 for first and last point against sides
+
+    final points = new List<double>.filled(pointsToGraph * 4, 0.0);
+
+    for (int i = 0; i < histogram.length - 1; ++i) {
+      points[i * 4] = (i * widthPerSample).toDouble();
+      points[i * 4 + 1] = size.height - (histogram[i].toDouble());
+      points[i * 4 + 2] = ((i + 1) * widthPerSample).toDouble();
+      points[i * 4 + 3] = size.height - (histogram[i + 1].toDouble());
+    }
+
+    Path path = new Path();
+    path.moveTo(0.0, size.height);
+    path.lineTo(points[0], points[1]);
+    for (int i = 2; i < points.length - 4; i += 2) {
+      path.cubicTo(
+        points[i - 2] + 10.0, points[i - 1],
+        points[i] - 10.0, points[i + 1],
+        points[i], points[i + 1]
+      );
+    }
+    path.lineTo(size.width, size.height);
+    path.close();
+
+    canvas.drawPath(path, wavePaint);
+  }
+
+  List<int> _createHistogram(List<int> samples, int bucketCount, [int start, int end]) {
+    if (start == end) {
+      return const [];
+    }
+
+    start = start ?? 0;
+    end =  end ?? samples.length;
+    final sampleCount = end - start + 1;
+
+    final samplesPerBucket = (sampleCount / bucketCount).floor();
+    if (samplesPerBucket == 0) {
+      return const [];
+    }
+
+    final actualSampleCount = sampleCount - (sampleCount % samplesPerBucket);
+    List<int> histogram = new List<int>.filled(bucketCount, 0);
+
+    // Ignore the 2nd item in the array because it represents the n/2 frequency value.
+    int loopStart = start;
+    if (start == 0) {
+      loopStart = 2;
+      histogram[0] += samples[0];
+    } else if (start == 1) {
+      loopStart = 2;
+    }
+
+    // Add up the frequency amounts for each bucket.
+    for (int i = loopStart; i <= start + actualSampleCount; ++i) {
+      // Ignore the imaginary half of each FFT sample
+      if ((i - loopStart) % 2 == 1) {
+        continue;
+      }
+
+      int bucketIndex = ((i - start) / samplesPerBucket).floor();
+      histogram[bucketIndex] += samples[i];
+    }
+
+    // Massage the data for visualization
+    for (var i = 0; i < histogram.length; ++i) {
+      histogram[i] = (histogram[i] / samplesPerBucket).abs().round();
+    }
+
+    return histogram;
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    return true;
+  }
+
 }
